d7abbf76b171807c8b8d0d038a671af5e9e0d7e2
Alex Shillingford <alex.shillingford@icloud.com>
2019-10-19 19:06:51 -0600

    Create SecondOnboardViewController.swift file

---
+++ GameAppQuestionMark/GameAppQuestionMark/View Controllers/SecondOnboardScrollViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondOnboardScrollViewController.swift
+//  GameAppQuestionMark
+//
+//  Created by Alex Shillingford on 10/19/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class SecondOnboardScrollViewController: UIViewController {
+    
+    let colors = Colors()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        refresh()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func refresh() {
+        view.backgroundColor = .clear
+        let backgroundLayer = colors.gl
+        backgroundLayer.frame = view.frame
+        view.layer.insertSublayer(backgroundLayer, at: 0)
+    }
+
+}
